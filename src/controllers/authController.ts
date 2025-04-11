5da66798f8432d566301dd477b555760a9b0aa7a
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-04-11 19:42:40 +0530

    Added login endpoint. Added bcrypt package to store the data in the encrpted format.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import User from '../models/User';
 import { generateToken } from '../helpers/jwt';
+import bcrypt from 'bcrypt';
 
 export const signupController = async(req: Request, res: Response, next: NextFunction) => {
     try {
@@ -22,11 +23,14 @@ export const signupController = async(req: Request, res: Response, next: NextFun
             throw createHttpError(400, "User already exists");
         }
 
+        //hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         //create user
         const user = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
             mobile: req.body.phone,
             profilePic: req.body.profilePic ?? null,
             interests: req.body.interests
@@ -41,6 +45,39 @@ export const signupController = async(req: Request, res: Response, next: NextFun
 
         //send response
         res.status(201).json({ success: true, message: "Signup successful", token, data:{ username: newUser?.username, email: newUser?.email  } });
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+    //login existing user
+export const loginUserController = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        //validate inputs
+        if(!req.body.email || !req.body.password){
+            throw createHttpError(400, "All fields are required");
+        }
+
+        //find user
+        const existingUser = await User.findOne({email: req.body.email});
+        console.log(existingUser?.password)
+        if(!existingUser){
+            throw createHttpError(400, "User does not exist");
+        }
+
+        //check password
+        const isPasswordValid = await bcrypt.compare(req.body.password, existingUser.password);
+        console.log(isPasswordValid);
+        if(!isPasswordValid){
+            throw createHttpError(400, "Invalid password");
+        }
+
+        //generate JWT token
+        const token = generateToken({email: existingUser.email, userID: String(existingUser._id)});
+
+        //send response
+        res.status(200).json({ success: true, message: "Login successful", token, data:{ username: existingUser?.username, email: existingUser?.email  } });
 
     } catch (error) {
         next(error)
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,11 @@
 const authRouter = require('express').Router();
 import { NextFunction, Request, Response } from 'express';
-import { signupController } from '../controllers/authController';
+import { loginUserController, signupController } from '../controllers/authController';
 
 //signup user
 authRouter.post('/signup', signupController);
+
+//login user
+authRouter.post("/login", loginUserController)
 
 export default authRouter;
